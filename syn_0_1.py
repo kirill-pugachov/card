13163f1e031fb7f2fdcbb17c5926b41c45b0491d
kirill-pugachov <pugachovk@gmail.com>
2018-09-21 06:51:54 +0300

    sparse columns with amount

--- syn_0_1.py
+++ syn_0_1.py
@@ -10,18 +10,33 @@ import itertools
 import copy
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn import preprocessing
+from sklearn.linear_model import SGDClassifier, LogisticRegression, LogisticRegressionCV
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVR, NuSVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 
 
 FILE_NAME = 'train_ds.csv'
-FILE_PATH = 'C:\\Users\\tom\\card_garbage\\Data\\'
+#FILE_PATH = 'C:\\Users\\tom\\card_garbage\\Data\\'
+FILE_PATH = 'Data/'
 TEST_FILE_NAME = 'test_ds.csv'
 CITIES_FILE_NAME = 'cities_list.csv'
+
 
 def get_train_data(FILE_NAME, FILE_PATH):
     temp = pd.read_csv(FILE_PATH + FILE_NAME)
     return temp
+
 
 def get_categorical_data():
     dtrain = get_train_data(FILE_NAME, FILE_PATH)
@@ -60,6 +75,76 @@ def feature_creator(users_list, ccy_tranccy_pairs):
                             result_dict[pair].update({unique_mcc:{1:0, 0: sum_by_mcc_user}})
     res = dict((key,value) for key, value in result_dict.items() if len(result_dict[key]) != 0)
     return res
+
+
+def classifiers_evaluation(df_res, y):
+
+    classifiers = [
+    LinearSVC(max_iter=2000),
+    LinearSVR(C=0.01, max_iter=2000, dual=False, loss='squared_epsilon_insensitive'),
+    KNeighborsClassifier(3),
+    SVC(probability=True),
+    NuSVC(),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(n_estimators=680),
+    AdaBoostClassifier(),
+    GradientBoostingClassifier(),
+    BernoulliNB(),
+    GaussianNB(),
+    LinearDiscriminantAnalysis(),
+    QuadraticDiscriminantAnalysis(),
+    LogisticRegression(),
+    MLPClassifier(hidden_layer_sizes=(110, ), max_iter=800),
+    SGDClassifier(loss='log', max_iter=800),
+    LogisticRegressionCV(max_iter=800)]
+
+    log_cols = ["Classifier", "ROC_AUC score"]
+    log = pd.DataFrame(columns=log_cols)
+
+#    quantile = preprocessing.QuantileTransformer(n_quantiles=2500)
+#    X = quantile.fit_transform(df_res)
+    
+#    minmax = preprocessing.MinMaxScaler()
+#    X = minmax.fit_transform(df_res)
+    
+#    norm = preprocessing.Normalizer()
+#    X = norm.fit_transform(df_res) 
+
+#    standart = preprocessing.StandardScaler()
+#    X = standart.fit_transform(df_res)
+    
+#    robust = preprocessing.RobustScaler()
+#    X = robust.fit_transform(df_res)  
+    
+    maxabs = preprocessing.MaxAbsScaler()
+    X = maxabs.fit_transform(df_res)    
+
+    sss = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
+
+    acc_dict = {}
+
+    for train_index, test_index in sss.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        for clf in classifiers:
+            name = clf.__class__.__name__
+            clf.fit(X_train, y_train)
+            train_predictions = clf.predict(X_test)
+            acc = roc_auc_score(y_test, train_predictions)
+
+            if name in acc_dict:
+                acc_dict[name] += acc
+            else:
+                acc_dict[name] = acc
+
+    for clf in acc_dict:
+        acc_dict[clf] = acc_dict[clf] / 10.0
+        log_entry = pd.DataFrame([[clf, acc_dict[clf]]], columns=log_cols)
+        log = log.append(log_entry)
+
+#    print(acc_dict)
+    print(log)
     
 
 if __name__ == '__main__':
@@ -97,23 +182,50 @@ if __name__ == '__main__':
     
     y_train_full = train_data_df['sexid']
     X_train_full = train_data_df.drop('sexid', axis=1)
-
-    X_train, X_test, y_train, y_test  =  train_test_split(
-                                            X_train_full,
-                                            y_train_full,
-                                            test_size = 0.25,
-                                            random_state=42
-                                            )
-        
-    model_0 = RandomForestClassifier(n_estimators=700, n_jobs=-1)
-    model_0.fit(X_train, y_train)
-    y_pred_forest_short = model_0.predict(X_test)
-    print(classification_report(y_test, y_pred_forest_short))
-    print(roc_auc_score(y_test, y_pred_forest_short))             
-#            train_data_df[str(U[0]) + '_' + str(U[1])+ '_' + str(U[2])][I] = users_list[I][users_list[I]['ccy'] == U[0] and users_list[I]['tranccy'] == U[1] and users_list[I]['mcc'] == U[2]]['amount'].sum()
-        
-        
     
+    classifiers_evaluation(X_train_full, y_train_full)
+
+#    X_train, X_test, y_train, y_test  =  train_test_split(
+#                                            X_train_full,
+#                                            y_train_full,
+#                                            test_size = 0.25,
+#                                            random_state=42
+#                                            )
+#        
+#    model_0 = RandomForestClassifier(n_estimators=700, n_jobs=-1)
+#    model_0.fit(X_train, y_train)
+#    y_pred_forest_short = model_0.predict(X_test)
+#    print(classification_report(y_test, y_pred_forest_short))
+#    print(roc_auc_score(y_test, y_pred_forest_short))
+#
+#    param_grid = [
+#        {'n_estimators':[640, 660, 680, 700, 1000, 2000, 5000],
+#        'max_depth':[1, 2, 3, 4, 5, 10, 20],
+#        'bootstrap':[True, False],
+#        'max_features':['auto', 'sqrt'],
+#        'min_samples_split': [2, 5, 10, 20, 50],
+#        'min_samples_leaf': [1, 2, 3, 4, 5]
+#        }
+#    ]
+#    
+#    forest_class = RandomForestClassifier()
+#    
+#    grid_search = GridSearchCV(forest_class, param_grid, cv=5, scoring='roc_auc', n_jobs=-1, verbose=2)
+#    
+#    grid_search.fit(X_train_full, y_train_full)
+#    
+#    forest_cls = grid_search.best_estimator_
+#    
+#    print('RandomForest best params', '\n', grid_search.best_params_)
+#    
+#    forest_cv_res = grid_search.cv_results_
+#    
+#    for mean_score, params in zip(forest_cv_res['mean_test_score'], forest_cv_res['params']):
+#        print(mean_score, params)        
+##            train_data_df[str(U[0]) + '_' + str(U[1])+ '_' + str(U[2])][I] = users_list[I][users_list[I]['ccy'] == U[0] and users_list[I]['tranccy'] == U[1] and users_list[I]['mcc'] == U[2]]['amount'].sum()
+#        
+#        
+#    
     
 #    result_dict = dict([(key,{}) for key in ccy_tranccy_pairs])
 #    
