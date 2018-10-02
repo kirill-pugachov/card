d063977ee76dc69b542d8762690ebe8bd8f5f947
kirill-pugachov <pugachovk@gmail.com>
2018-10-02 18:03:12 +0300

    fitting NN

--- temp_five.py
+++ temp_five.py
@@ -5,32 +5,37 @@ Created on Sun Sep 30 21:43:16 2018
 @author: User
 """
 
+
+from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.layers import Embedding
-from keras.layers import LSTM
+from keras import regularizers
+#from keras.layers import Embedding
+#from keras.layers import LSTM
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
 
 
-FILE_PATH = 'Data/'
-X_FILE_NAME = 'X_train_full_2809.csv'
-Y_FILE_NAME = 'y_train_2809.csv'
+#FILE_PATH = 'Data/'
+FILE_PATH = 'C:\\Users\\tom\\card_garbage\\Data\\'
+X_FILE_NAME = 'new_dataframe_1.csv'
+
 
 
 # define baseline model
-def baseline_model():
-	# create model
-	model = Sequential()
-	model.add(Dense(8, input_dim=1492, activation='relu'))
-	model.add(Dense(1, activation='sigmoid'))
-	# Compile model
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+def baseline_model(optimizer='adam'):
+    model = Sequential()
+    model.add(Dense(32, input_dim=268, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(16, activation='relu', kernel_regularizer=regularizers.l2(0.009)))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
 
 
 if __name__ == '__main__':
@@ -40,25 +45,50 @@ if __name__ == '__main__':
 #    train_labels = np.genfromtxt(FILE_PATH + Y_FILE_NAME, delimiter=",")[1:]
     
     X_train_full = pd.read_csv(FILE_PATH + X_FILE_NAME)
-    y_train = pd.read_csv(FILE_PATH + Y_FILE_NAME, header=None)
+    y_train = X_train_full['sexid']
     
     X_train_full.drop(['Unnamed: 0'], axis=1, inplace=True)
-    y_train.drop([0], axis=1, inplace=True)
+    X_train_full.drop(['sexid'], axis=1, inplace=True)
+#    y_train.drop([0], axis=1, inplace=True)
     
-    X_train_full_norm = X_train_full.div(X_train_full.sum(axis=1), axis=0)
+#    X_train_full_norm = X_train_full.div(X_train_full.sum(axis=1), axis=0)
     
-    dataset = X_train_full_norm.values
+    X_train_full = X_train_full.apply(np.log)
+    X_train_full[np.isneginf(X_train_full)] = 0
     
-    X = dataset[:,0:1492].astype(float)
+    norm = preprocessing.Normalizer()
+    X_train_full_norm = norm.fit_transform(X_train_full)
+    
+#    dataset = X_train_full_norm.values
+    dataset = X_train_full_norm
+    
+    X = dataset[:,0:268].astype(float)
     y = y_train.values
     
-    estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=1)
+    estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=10, verbose=0)
     
     kfold = KFold(n_splits=10, shuffle=True, random_state=42)
     
     results = cross_val_score(estimator, X, y, cv=kfold)
     print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
     
+    model = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=10, verbose=0)
+    
+    optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+    param_grid = dict(optimizer=optimizer)
+    
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+    grid_result = grid.fit(X, y)
+    
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+    
+
 #    scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 #    X_train_min = scaler.fit_transform(X_train_full)
     
